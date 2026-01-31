eef8e654a49294f80baf27ba37c0d2c71ff52af4
lawsbb <lawsbb@rose-hulman.edu>
2026-01-31 13:12:08 -0500

    finished direction, player, and zombie

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,5 +1,37 @@
 package model;
 
-public class Player {
+import java.awt.event.KeyEvent;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+
+
+public class Player extends Entity{
+
+	public Player(int row, int col, Maze maze) {
+		super(row, col, maze);
+		
+		try {
+			
+			sprite = ImageIO.read(Player.class.getResource("pngkit_game-character-png_106630.png"));
+			} catch (IOException | IllegalArgumentException ex) {
+			sprite = null; 
+			}
+			}
+	
+	public void handleKey(KeyEvent e) {
+		if (e.getKeyCode() ==KeyEvent.VK_W) {
+			move(Direction.UP);
+		}else if (e.getKeyCode() ==KeyEvent.VK_S) {
+			move(Direction.DOWN);
+		}else if (e.getKeyCode() ==KeyEvent.VK_A) {
+			move(Direction.LEFT);
+		}else if (e.getKeyCode() == KeyEvent.VK_D) {
+			move(Direction.RIGHT);
+	}
+	}
 }
+
+	
+
