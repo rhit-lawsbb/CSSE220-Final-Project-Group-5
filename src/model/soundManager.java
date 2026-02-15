f9e5072df85ac0ef7e746910a58e0b2d4aab80a7
lawsbb <lawsbb@rose-hulman.edu>
2026-02-15 17:09:37 -0500

    ball

--- src/model/soundManager.java
+++ src/model/soundManager.java
@@ -1,22 +1,7 @@
 package model;
 
+
+
 public class soundManager {
-	public void playSound(String fileName) {
-	    new Thread(() -> {
-	        try {
-	            
-	            java.net.URL url = getClass().getResource("/sounds/" + fileName);
-	            if (url == null) {
-	                System.err.println("Could not find: " + fileName);
-	                return;
-	            }
-	            javax.sound.sampled.AudioInputStream audioIn = javax.sound.sampled.AudioSystem.getAudioInputStream(url);
-	            javax.sound.sampled.Clip clip = javax.sound.sampled.AudioSystem.getClip();
-	            clip.open(audioIn);
-	            clip.start();
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-	    }).start();
-	}
+	
 }
