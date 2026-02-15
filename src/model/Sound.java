f58b69ccdf8f025889ece87dae77530d29039002
lawsbb <lawsbb@rose-hulman.edu>
2026-02-15 16:39:06 -0500

    sound

--- src/model/Sound.java
+++ src/model/Sound.java
@@ -1,5 +1,27 @@
 package model;
 
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
 public class Sound {
+	public static void playSound(String fileName) {
+        try {
+            // This approach works whether the sound is in a folder or inside a JAR
+            URL url = Sound.class.getResource("/sounds/" + fileName);
+            if (url == null) {
+                System.err.println("Could not find file: " + fileName);
+                return;
+            }
+            
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
