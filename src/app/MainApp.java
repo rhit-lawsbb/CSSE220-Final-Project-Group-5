e634751d40ec4a3938b746231215ba5a376ab8b0
turneras <turners@rose-hulman.edu>
2026-02-19 16:07:45 -0500

    package app javadoc complete

--- src/app/MainApp.java
+++ src/app/MainApp.java
@@ -12,18 +12,25 @@ import ui.GameWindow;
  * Launches game window and starts the ui overlay
  * <br>Restrictions: No game or render logic is in this class,
  * only responsibility in this class is starting the app
- * <br> for example:
+ * <br>For example:
+ * <pre>
  * MainApp.main(new String[]{});
- * 
+ * </pre>
  */
 public class MainApp {
-	
+	/**
+	 * ensures: launches the app by calling to create the ui
+	 * @param args command-line arguments
+	 */
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(() -> {
 		new MainApp().run();
 		});
 		}
 	
+	/**
+	 * ensures: creates and shows the game window
+	 */
 	public void run() {
 		GameWindow.show();
 		// Hint: MainApp should not contain game logic or drawing code
