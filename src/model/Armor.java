4dfb7e339ef18406b924cf4c6b8c6caae2c5a0a4
turneras <turners@rose-hulman.edu>
2026-02-19 19:59:16 -0500

    armor and bullet java doc

--- src/model/Armor.java
+++ src/model/Armor.java
@@ -5,10 +5,27 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
-// test
+/**
+ * Class: Armor
+ * @author Group 5
+ * <br>Purpose: Represents armor pickup that can reduce incoming damage from zombie collision with the player. 
+ * <br>Restrictions: No collision logic is contained in this class, as it is handled in CollisionHandler. 
+ * Only handles the armor in visual and spatial ways.
+ * <br>For example:
+ * <pre>
+ * 		Armor armor = new Armor(1,2);
+ * </pre>
+ */
 
 public class Armor extends Pickup {
 
+	/**
+	 * ensures: creates an armor pick up on a specified location on the map
+	 * and attempts to load the sprite.
+	 * If the sprite load fails, the sprite is set to null.
+	 * @param col column location on the map of the armor pickup
+	 * @param row row location on the map of the armor pickup
+	 */
 	public Armor(int col, int row) {
 		super(col, row);
 		try {
--- src/model/Bullet.java
+++ src/model/Bullet.java
@@ -2,6 +2,18 @@ package model;
 
 import java.awt.Color;
 import java.awt.Graphics;
+
+/**
+ * Class: Bullet
+ * @author Group 5
+ * <br>Purpose: Represents a projectile fired from the player.
+ * handles the bullets movement and when it collides a wall
+ * <br>Restrictions: Once fired, the bullet is deactivated when collision with wall is detected.
+ * <br>For example:
+ * <pre>
+ * 		Bullet bullet = new Bullet(x,y,true,maze);
+ * </pre>
+ */
 public class Bullet {
     private double x, y;
     private Direction direction;
@@ -9,6 +21,16 @@ public class Bullet {
     private Maze maze;
     private static final int SPEED = 12;
     private static final int SIZE = 10;
+    
+    /**
+     * ensures: creates a bullet at a specified coordinate location on the map, 
+     * decides direction based on facingRight boolean param,
+     * declares bullet as active
+     * @param startX starting x coordinate of the bullet
+     * @param startY starting y coordinate of the bullet
+     * @param facingRight true if the bullet should be moving right, false if left
+     * @param maze the maze of the level, needed to detect walls
+     */
     public Bullet(double startX, double startY, boolean facingRight, Maze maze) {
         this.maze = maze;
         this.x = startX + 20; // spawn near player's center
@@ -19,6 +41,12 @@ public class Bullet {
             direction = Direction.LEFT;
         }
     }
+    
+    /**
+     * ensures: moves the bullet in the specified direction.
+     * if bullet hits a wall, then it becomes inactive
+     * if inactive, no bullet movement occurs.
+     */
     public void update() {
         if (!active) return;
         double nextX = x + direction.getDx() * SPEED;
@@ -32,13 +60,38 @@ public class Bullet {
         x = nextX;
         y = nextY;
     }
+    /**
+     * ensures: if bullet is active, draws bullet on screen
+     * if inactive, nothing is drawn
+     * @param g graphics used for rendering
+     * <br>requires: g &ne; null
+     */
     public void draw(Graphics g) {
         if (!active) return;
         g.setColor(Color.YELLOW);
         g.fillOval((int) x, (int) y, SIZE, SIZE);
     }
+    /**
+     * ensures: returns if the bullet is active or not
+     * @return true if bullet is active, false if inactive
+     */
     public boolean isActive() { return active; }
+    
+    /**
+     * ensures: sets bullet to inactive
+     * once inactive, the bullet wont be drawn or move
+     */
     public void deactivate() { active = false; }
+    
+    /**
+     * returns the x coordinate of the bullet
+     * @return x coordinate of bullet
+     */
     public double getX() { return x; }
+    
+    /**
+     * returns the y coordinate of the bullet
+     * @return y coordinate of the bullet
+     */
     public double getY() { return y; }
 }
